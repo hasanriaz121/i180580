29e015830983bce8a5ce62820f46f33096558723
hasanriaz121 <75634716+hasanriaz121@users.noreply.github.com>
2022-09-14 12:48:48 +0500

    Sign In & Sign Up pages without logo

---
+++ app/src/main/java/com/hassanriaz/i180580/AltSignUp.java
@@ -0,0 +1,14 @@
+package com.hassanriaz.i180580;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AltSignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alt_sign_up);
+    }
+}
